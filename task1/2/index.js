519dcabe54d96f983a545c07ebccd643940cf2ec
IvanKurtts <ivankurt96@gmail.com>
2022-12-21 00:05:56 +0300

    task1.2 done

--- task1/2/index.js
+++ task1/2/index.js
@@ -1,12 +1,18 @@
 function task2() {
     let a = +prompt('Введите первое значение');
-    let b = +prompt('Введите второе значение');
     
-    if (!Number.isInteger(a) || !Number.isInteger(b) || b>36 || b<2) {
+    if (!Number.isFinite(a) || Number.isNaN(a)) {
         alert('Некорректный ввод!');
         console.log('Некорректный ввод!');
         return;
         } else {
-        alert(a.toString(b));
+            let b = +prompt('Введите второе значение');
+            if (!Number.isFinite(b) || Number.isNaN(b)) {
+                alert('Некорректный ввод!');
+                console.log('Некорректный ввод!');
+                return;
+            } else {
+                alert (`Ответ: ${a+b}, ${a-b}`)
+            }
         }
     }
