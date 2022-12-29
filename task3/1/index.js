4636010a2850496da2d39232456eb8f0341523bf
IvanKurtts <ivankurt96@gmail.com>
2022-12-29 16:43:23 +0300

    task3.1 done

--- task3/1/index.js
+++ task3/1/index.js
@@ -1,25 +1,34 @@
-// const myIterable = { from: 1, to: 7 };
-// const myIterable = { from: 'aaa', to: 4 };
+// let arr = ['sgsb', 5, 'sdfbb', 87, 'sdf', 'sggdf', 66, 'aa', -8, 10]
 
-if (typeof myIterable.from === 'number' && 
-    typeof myIterable.to === 'number' && myIterable.from<=myIterable.to) {
-
-    myIterable[Symbol.iterator] = function() {
-        return {
-            current: this.from,
-            last: this.to,
-
-            next() {
-                if (this.current <= this.last) {
-                return { done: false, value: this.current++ };
-                } else {
-                return { done: true };
-                }
-            }
-        };
-    };
-} else {throw new Error('Ошибка!')}
-
-for (let item of myIterable) {
-console.log(item);
-}
+Array.prototype.myFilter = function (callback, thisArg) {
+    if (typeof callback !== "function") {
+      throw new Error("Ошибка!");
+    }
+    
+    if (arguments.length > 1) {
+      this.arr = thisArg;
+    }
+  
+    let result = [];
+  
+    for (let i = 0; i < this.length; i++) {
+      if (i in arr) {
+        if (callback.call(this.arr, this[i], i, arr)) {
+          result.push(this[i]);
+        }
+      }
+    }
+    return result;
+  };
+  
+//   console.log(arr.myFilter(function(item, index, array) {return typeof item === 'string'}));
+//   console.log(arr.filter(function(item, index, array) {return typeof item === 'string'}));
+//   console.log(arr.myFilter(function(item, index, array) {return typeof item === 'number'}));
+//   console.log(arr.filter(function(item, index, array) {return typeof item === 'number'}));
+//   console.log(arr.myFilter(function(item, index, array) {return item%2 === 0}));
+//   console.log(arr.filter(function(item, index, array) {return item%2 === 0}));
+//   console.log(arr.myFilter(function(item, index, array) {return index === array.length-1}));
+//   console.log(arr.filter(function(item, index, array) {return index === array.length-1}));
+  
+  
+  
